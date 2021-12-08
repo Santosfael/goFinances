a0fb0cc53a2bd03e0548f7c2003274b8214fa73d
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-12-07 21:22:43 -0300

    Inicializando a utilizaçao de proporçoes em medidas

--- App.tsx
+++ App.tsx
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import AppLoading from 'expo-app-loading';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+
+import { Dashboard } from './src/screens/Dashboard';
+import theme from './src/global/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ThemeProvider theme={theme}>
       <StatusBar style="auto" />
-    </View>
+      <Dashboard />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {
+    Container,
+    Header,
+} from './styles';
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Header>
+            </Header>
+        </Container>
+    );
+}
