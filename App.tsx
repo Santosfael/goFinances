0663dc9f23737bb1f8c297e6903ac293bf8692d7
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-12-08 19:10:59 -0300

    finalizing header and cards styling

--- App.tsx
+++ App.tsx
@@ -26,7 +26,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
       <Dashboard />
     </ThemeProvider>
   );
