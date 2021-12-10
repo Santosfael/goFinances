d9345a3cfee2ea3d25ae2b2cc4f63e5f3560d945
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-12-10 19:44:32 -0300

    Including in-app navigation, Refactoring and using the rectbutton and borderlessButton - completing forms and navigation module

--- src/components/Form/TransactionTypeButton/index.tsx
+++ src/components/Form/TransactionTypeButton/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { TouchableOpacityProps } from 'react-native';
+import { RectButtonProps } from 'react-native-gesture-handler';
 
 import {
     Container,
+    Button,
     Icon,
     Title
 } from './styles';
@@ -12,7 +13,7 @@ const icons = {
     down: 'arrow-down-circle'
 }
 
-interface Props extends TouchableOpacityProps {
+interface Props extends RectButtonProps {
     title: string;
     type: 'up' | 'down';
     isActive: boolean;
@@ -21,17 +22,20 @@ interface Props extends TouchableOpacityProps {
 export function TransactionTypeButton({ type, title, isActive, ...rest }: Props) {
     return (
         <Container
-            {...rest}
             isActive={isActive}
             type={type}
         >
-            <Icon
-                name={icons[type]}
-                type={type}
-            />
-            <Title>
-                {title}
-            </Title>
+            <Button
+                {...rest}
+            >
+                <Icon
+                    name={icons[type]}
+                    type={type}
+                />
+                <Title>
+                    {title}
+                </Title>
+            </Button>
         </Container>
     );
 }
