23ec6db37bb060ecca54213338138a3fcce2fc67
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-12-25 11:00:04 -0300

    First test

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -3,10 +3,28 @@ import { render } from '@testing-library/react-native';
 
 import { Profile } from '../../screens/Profile';
 
-test('check if sho correctly user input name placeholder', () => {
+test('check if show correctly user input name placeholder', () => {
     const { getByPlaceholderText } = render(<Profile />);
 
     const inputName = getByPlaceholderText('Nome');
     
-    expect(inputName.props.placeholder).toBeTruthy();
+    expect(inputName).toBeTruthy();
+});
+
+test('check if user data has been loader', () => {
+    const { getByTestId } = render(<Profile />);
+
+    const inputName = getByTestId('input-name');
+    const inputSurname = getByTestId('input-surname');
+    
+    expect(inputName.props.value).toEqual('Rafael');
+    expect(inputSurname.props.value).toEqual('Rocha dos Santos');
+});
+
+test('check if title render correctly', () => {
+    const { getByTestId } = render(<Profile />);
+
+    const textTitle = getByTestId('text-title');
+
+    expect(textTitle.props.children).toContain('Perfil');
 });
